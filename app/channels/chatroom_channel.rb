746c8ee23260e0ac2f3cf15c163be58abef3a800
Luisa Egoavil <lupe.egoavil@gmail.com>
2022-04-11 12:29:46 +0200

    modify and broadcast messages

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "some_channel"
+    stream_from 'chatroom_channel'
   end
 
   def unsubscribed
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,16 @@
-class MessagesController < ApplicationController 
-    before_action :require_user 
+# frozen_string_literal: true
 
-    def create  
-        message = current_user.messages.build(message_params) 
-        if message.save 
-            redirect_to root_path
-        end
-     end  
+class MessagesController < ApplicationController
+  before_action :require_user
 
-    private 
-    
-    def message_params 
-        params.require(:message).permit(:body) 
-    end
+  def create
+    message = current_user.messages.build(message_params)
+    ActionCable.server.broadcast 'chatroom_channel', foo: message.body if message.save
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
 end
